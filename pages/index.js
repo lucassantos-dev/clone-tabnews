a1c37b199e7aa0b4042b61e49bb0e63f94985120
lucas.santos <jl.lucassantos098@gmail.com>
2025-07-23 20:07:04 -0300

    adiciona arquivo .editorconfig e atualiza dependências no package.json e package-lock.json

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Renata, eu amo você. Se você me ama da uma risadinha.</h1>
+  return <h1>Renata, eu amo você. Se você me ama da uma risadinha.</h1>;
 }
-export default Home
+export default Home;
